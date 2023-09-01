20951d521531694c395846cdb17326663d2b924c
nirantarwe2codenorthern <nirantar@we2code.com>
2023-09-01 17:37:33 +0530

    four

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React dsad dsa
+         click and get
         </a>
       </header>
     </div>
